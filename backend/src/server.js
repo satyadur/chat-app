89557212064993d3bb1aff186240ed9b1c5b053c
satyadur <satyakudupudi8500@gmail.com>
2025-11-01 13:21:22 +0530

    second commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,6 +24,13 @@ app.use(
 );
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "🚀 Server started successfully and is running fine!",
+  });
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
